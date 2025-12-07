143e3a95cdbbab4801f8e72a902babee1f36c295
Akuir98 <artyrkugut98@gmail.com>
2025-12-08 00:55:00 +1000

    Add conf.lua for the level editor configuration

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.title = "Baller - Level Editor"
+    t.window.width = 1280
+    t.window.height = 800
+    t.window.vsync = 1
+    t.window.resizable = true
+    t.window.minwidth = 800
+    t.window.minheight = 600
+end
